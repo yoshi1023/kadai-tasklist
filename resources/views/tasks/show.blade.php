e30580ac39a32bcb239d0d51a25489a0aa11f9b3
EC2 Default User <ec2-user@ip-172-31-10-103.us-east-2.compute.internal>
2018-03-06 10:14:26 +0000

    CRUD tasklist

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id = {{ $task->id }} 詳細ページ</h1>
+    
+    <p>{{ $task->content }}</p>
+    
+    {!! link_to_route('tasks.edit', '編集', ['id' => $task->id]) !!}
+    
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+
+@endsection
