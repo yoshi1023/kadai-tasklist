8b211f63b3d16d5f9f4810cd7a270c758a8ad4ec
EC2 Default User <ec2-user@ip-172-31-10-103.us-east-2.compute.internal>
2018-03-13 15:47:49 +0000

    bootstrap3

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,28 +2,27 @@
 
 @section('content')
 
-    <h1>作成ページ</h1>
-    
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    <!--<h1>作成ページ</h1>-->
         
         <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
             
-                <div class="form-group">
-                    {!! Form::label('content', 'タスク:') !!}
-                    {!! Form::text('content', null, ['class' => 'form-control'] ) !!}
-                </div>
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+                <h1>作成ページ</h1>
+                {!! Form::model($task, ['route' => 'tasks.store']) !!}
                 
-                <div class="form-group">
-                    {!! Form::label('status', 'ステータス:') !!}
-                    {!! Form::text('status', null, ['class' => 'form-control'] ) !!}
-                </div>
-                
+                    <div class="form-group">
+                        {!! Form::label('content', 'タスク:') !!}
+                        {!! Form::text('content', null, ['class' => 'form-control'] ) !!}
+                    </div>
+                    
+                    <div class="form-group">
+                        {!! Form::label('status', 'ステータス:') !!}
+                        {!! Form::text('status', null, ['class' => 'form-control'] ) !!}
+                    </div>
+                    {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+                {!! Form::close() !!}
             </div>
         </div>
 
-        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
-
-    {!! Form::close() !!}
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,12 +2,10 @@
 
 @section('content')
     
-    <h1>id: {{ $task->id }} の編集ページ</h1>
-    
     <div class="row">
         
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
-            
+            <h1>id: {{ $task->id }} の編集ページ</h1>
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
                 
                 <div class="form-group">
