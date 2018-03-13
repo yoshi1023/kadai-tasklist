6583d96c2bd949251bb01a88025ee7eb273e3dd5
EC2 Default User <ec2-user@ip-172-31-10-103.us-east-2.compute.internal>
2018-03-13 09:16:25 +0000

    offset

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -7,7 +7,7 @@
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
         
         <div class="row">
-            <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
             
                 <div class="form-group">
                     {!! Form::label('content', 'タスク:') !!}
